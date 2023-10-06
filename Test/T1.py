96be9deb8b8128d4175816b0acf20a38c1086135
Charindu Ranasinghe <99631065+CharinduRanasinghe@users.noreply.github.com>
2023-10-06 12:47:50 +0530

    Modified to use opencv instead

--- Test/T1.py
+++ Test/T1.py
@@ -1,6 +1,9 @@
+import cv2
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageOps
+from tkinter import simpledialog, messagebox
+from PIL import Image, ImageTk
+import numpy as np
 
 class ImageProcessorApp:
     def __init__(self, master):
@@ -28,32 +31,44 @@ class ImageProcessorApp:
     def load_image(self):
         file_path = filedialog.askopenfilename()
         if file_path:
-            self.image = Image.open(file_path)
+            self.image = cv2.imread(file_path)
             self.display_image()
 
     def display_image(self):
-        self.tk_image = ImageTk.PhotoImage(self.image)
-        self.image_label.configure(image=self.tk_image)
-
+        if self.image is not None:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+            img_pil = Image.fromarray(self.image)
+            self.tk_image = ImageTk.PhotoImage(image=img_pil)
+            self.image_label.configure(image=self.tk_image)
+    
     def rotate_image(self):
         if hasattr(self, 'image'):
-            self.image = self.image.rotate(45)
+            rows, cols, _ = self.image.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)
+            self.image = cv2.warpAffine(self.image, M, (cols, rows))
             self.display_image()
 
     def crop_image(self):
         if hasattr(self, 'image'):
-            self.image = self.image.crop((100, 100, 400, 400))
+            self.image = self.image[100:400, 100:400]
             self.display_image()
 
     def flip_image(self):
         if hasattr(self, 'image'):
-            self.image = ImageOps.flip(self.image)
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
             self.display_image()
 
     def resize_image(self):
         if hasattr(self, 'image'):
-            self.image = self.image.resize((self.image.width // 2, self.image.height // 2))
-            self.display_image()
+            user_input = simpledialog.askstring("Resize Image", "Enter width and height (e.g., '300 200'):")
+            if user_input:
+                try:
+                    width, height = map(int, user_input.split())
+                    self.image = cv2.resize(self.image, (width, height))
+                    self.display_image()
+                except ValueError:
+                    messagebox.showerror("Error", "Invalid input. Please enter valid width and height.")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
